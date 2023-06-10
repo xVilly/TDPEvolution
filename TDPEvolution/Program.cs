b36e3a3521c2ada956ecd52c99afdc38522d03a2
xVilly <mich038al@gmail.com>
2023-06-10 18:47:35 +0200

    Add project files.

---
+++ TDPEvolution/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TDPEvolution
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
